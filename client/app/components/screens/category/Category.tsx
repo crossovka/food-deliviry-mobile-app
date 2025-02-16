567450ec18942a8c10310c105c19f7530e1929e7
crossovka <best-login-mail@yandex.ru>
2025-02-16 19:14:11 +0900

    make category page

--- client/app/components/screens/category/Category.tsx
+++ client/app/components/screens/category/Category.tsx
@@ -2,11 +2,23 @@ import { FC } from 'react'
 import { Text } from 'react-native'
 
 import Layout from '@/components/layout/Layout'
+import Loader from '@/components/ui/Loader'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useCategory } from './useCategory'
 
 const Category: FC = () => {
+	const { category, products, isLoading } = useCategory()
+
+	if (isLoading) return <Loader />
+
 	return (
 		<Layout>
-			<Text>Category</Text>
+			{category ? (
+				<Catalog title={category.name} products={products || []} />
+			) : (
+				<Text>Category not found</Text>
+			)}
 		</Layout>
 	)
 }
