1e76bf0142895c0bd9504b91f8ce6be35e4ea13d
crossovka <best-login-mail@yandex.ru>
2025-02-16 19:09:26 +0900

    make explorer screen

--- client/app/components/screens/explorer/Explorer.tsx
+++ client/app/components/screens/explorer/Explorer.tsx
@@ -1,12 +1,21 @@
 import { FC } from 'react'
-import { Text } from 'react-native'
 
 import Layout from '@/components/layout/Layout'
+import Loader from '@/components/ui/Loader'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useGetAllProducts } from './useGetAllProducts'
 
 const Explorer: FC = () => {
+	const { products, isLoading } = useGetAllProducts()
+
 	return (
 		<Layout>
-			<Text>Explorer</Text>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<Catalog title='Explorer' products={products || []} />
+			)}
 		</Layout>
 	)
 }
