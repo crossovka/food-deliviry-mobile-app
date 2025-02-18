23baf93c34530fa644908abf98354a6f0c7cd287
crossovka <best-login-mail@yandex.ru>
2025-02-18 19:13:13 +0900

    favorites page

--- client/app/components/screens/favorites/Favorites.tsx
+++ client/app/components/screens/favorites/Favorites.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react'
-import { Text } from 'react-native'
 
 import Layout from '@/components/layout/Layout'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useProfile } from '../profile/useProfile'
 
 const Favorites: FC = () => {
+	const { profile } = useProfile()
+
 	return (
 		<Layout>
-			<Text>Favorites</Text>
+			<Catalog title='Favorites' products={profile?.favorites || []} />
 		</Layout>
 	)
 }
