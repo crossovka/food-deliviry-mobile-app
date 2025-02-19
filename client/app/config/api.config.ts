e80801cf08ff3ffae5c95940ebbadb5fffa5e4e3
crossovka <best-login-mail@yandex.ru>
2025-02-19 19:04:48 +0900

    add stripe N redux provider

--- client/app/config/api.config.ts
+++ client/app/config/api.config.ts
@@ -1,6 +1,7 @@
 export const SERVER_URL = process.env.SERVER_URL
 // export const API_URL = `${SERVER_URL}/api`
 export const API_URL = `http://192.168.122.35:4200/api`
+console.log(`API_URL ${API_URL}`)
 
 export const getAuthUrl = (string: string) => `/auth${string}`
 export const getUsersUrl = (string: string) => `/users${string}`
